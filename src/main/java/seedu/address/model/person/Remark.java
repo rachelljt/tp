4a31ee63f21f164475deb4d3f0245431575597be
rachel <rachel.ljt@gmail.com>
2021-02-26 17:38:31 +0800

    checkstyle errors

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Remark constructor that instantiates a Remark object
+     *
+     * @param remark the remark made by the user
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
